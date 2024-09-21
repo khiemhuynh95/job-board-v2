99d9c6b16d58e2d4537116bf279d13c13bce565a
khiemhuynh95 <khiemhuynh952@gmail.com>
2024-09-21 13:37:41 -0500

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx', './public/**/*.svg'],
-  theme: {},
-  future: {
-    hoverOnlyWhenSupported: true,
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
